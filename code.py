4687ca758693c8381b13de2c08f58bd82e0d618f
geekboysupreme <shuvam.manna@gmail.com>
2019-07-21 20:39:19 +0530

    updates

--- code.py
+++ code.py
@@ -1,7 +1,10 @@
+#This function is not triggered right now due to a routing issue
 def getresponse():
-  response = "Hello from the other side"
+  response = "Staging Folder/File for upload..."
   return response
 
+#This function works, the insides can be modified to do the basic job of staging the photographs from one pre-defined filename for upload
+#the App currently cannot take in custom inouts due to routing issues, looking to resolve them.
 def getresponse2():
   response1 = "This is the second Response"
   return response1
--- server.py
+++ server.py
@@ -5,16 +5,16 @@ from code import getresponse, getresponse2
 app = Flask(__name__)
 CORS(app)
 
-response = getresponse()
-response1 = getresponse2()
 
-@app.route('/hello')
-def get_response():
-  return response
-
+#extra routes resulting 404 errors for reasons unknown
+#an array created here shall take in and store the user variables
 @app.route('/hi')
 def get_response1():
-  return response1
+  return getresponse2()
+
+@app.route('/hither')
+def get_response():
+  return getresponse()
 
 if __name__ == "__main__":
     app.run()
