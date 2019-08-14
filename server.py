3f0cf55d362bf20e744bd5b8ca3275d87e95d161
geekboysupreme <shuvam.manna@gmail.com>
2019-08-14 21:41:19 +0530

    Updates

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
-from flask import Flask, request
+from flask import Flask
 from flask_cors import CORS
 from code import getresponse, getresponse2
-
 app = Flask(__name__)
 CORS(app)
 
@@ -10,16 +9,11 @@ CORS(app)
 #an array created here shall take in and store the user variables
 @app.route('/hi')
 def get_response1():
-  return getresponse2()
+  return getresponse2('haha')
 
 @app.route('/hello')
 def get_response():
   return getresponse()
-
-@app.route('/exit')
-def get_response2():
-  return getresponse3()
-
 
 if __name__ == "__main__":
     app.run()
---
+++ test.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_cors import CORS
+from code import getresponse, getresponse2
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/hello/<name>')
+def route_response(name):
+   return getresponse2()
+
+
+if __name__ == '__main__':
+   app.run()
